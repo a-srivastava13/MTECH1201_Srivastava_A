0ead8686e3138e70ced915df65bbbf8c95e0419e
a-srivastava13 <andy.srivastava13@gmail.com>
2025-09-10 12:41:24 -0400

    Uploaded Homework

---
+++ Homework/Homework 1/sketch.js
@@ -0,0 +1,73 @@
+// Andy S
+// Lone Flower in a Field
+//Honestly this is just me messing around
+//trying to learn the functions of p5.js
+
+
+
+function setup() {
+  createCanvas(800, 500);
+  background(131,222,242)
+
+  noStroke();
+  fill(101,184,50)
+  rect(0,400,800,100)
+
+  fill(237,204,17);
+  circle(725,60, 100);
+
+  let c = color(237,204,17)
+  c.setAlpha(128);
+  fill(c);
+  noSmooth();
+  circle(725,60, 200);
+
+  fill(255,255,255);
+  ellipse(100,150,75,50);
+
+  fill(255,255,255);
+  ellipse(150,150,75,50);
+
+  fill(255,255,255);
+  ellipse(125,125,60,50);
+
+  stroke(55,79,47);
+  strokeWeight(5);
+  line(400, 430, 400, 375);
+
+  stroke(0);
+  strokeWeight(1);
+  fill(255,255,255);
+  circle(391,365,18);
+
+  fill(255,255,255);
+  circle(400,358,18);
+
+  fill(255,255,255);
+  circle(410,365,18);
+
+  fill(255,255,255);
+  circle(408,375,18);
+
+  fill(255,255,255);
+  circle(396,376,18);
+  
+  
+  noStroke();
+  fill(255,0,0);
+  circle(400,370,15);
+
+  
+
+  
+
+}
+
+function draw() {
+
+  noFill();
+  strokeWeight(2);
+
+  
+  
+}
